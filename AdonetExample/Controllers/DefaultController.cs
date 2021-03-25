148071b3fdabac3a176b266b1a57921ef4e22479
yawar2019 <yawarali17@gmail.com>
2021-03-25 23:07:59 +0530

    Committed

--- AdonetExample/Controllers/DefaultController.cs
+++ AdonetExample/Controllers/DefaultController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using AdonetExample.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -9,7 +10,17 @@ namespace AdonetExample.Controllers
     public class DefaultController : Controller
     {
         // GET: Default
+        EmployeeContext db = new EmployeeContext();
         public ActionResult Index()
+        {
+            return View(db.getEmployees());
+        }
+        public ActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Create(EmployeeModel emp)
         {
             return View();
         }
--- AdonetExample/Models/EmployeeModel.cs
+++ AdonetExample/Models/EmployeeModel.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Data.SqlClient;
 using System.Linq;
 using System.Web;
@@ -20,9 +21,19 @@ namespace AdonetExample.Models
         public List<EmployeeModel> getEmployees()
         {
             List<EmployeeModel> objlist = new List<EmployeeModel>();
-        
-
-
+            SqlCommand cmd = new SqlCommand("spr_getEmployeeDetails", con);
+            cmd.CommandType =  CommandType.StoredProcedure;
+            SqlDataAdapter da = new SqlDataAdapter(cmd);
+            DataTable dt = new DataTable();
+            da.Fill(dt);
+            foreach (DataRow dr in dt.Rows)
+            {
+                EmployeeModel emp = new EmployeeModel();
+                emp.EmpId = Convert.ToInt32(dr[0]);
+                emp.EmpName = Convert.ToString(dr[1]);
+                emp.EmpSalary = Convert.ToInt32(dr[2]);
+                objlist.Add(emp);
+            }
 
             return objlist;
         }
