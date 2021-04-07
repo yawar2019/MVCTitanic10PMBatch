d3098590ee6c6c0a6a2e91c7a438a400e293ceaf
yawar2019 <yawarali17@gmail.com>
2021-04-07 23:07:50 +0530

    committed

---
+++ CodeFirstApproach/Controllers/DefaultController.cs
@@ -0,0 +1,60 @@
+﻿using CodeFirstApproach.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Data.Entity;
+namespace CodeFirstApproach.Controllers
+{
+    public class DefaultController : Controller
+    {
+        // GET: Default
+        EmployeeContext db = new EmployeeContext();
+        public ActionResult Index()
+        {
+            return View(db.EmployeeModels.ToList());
+        }
+
+        public ActionResult Create() {
+
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Create(EmployeeModel emp)
+        {
+            db.EmployeeModels.Add(emp);//line creating Insert Query
+            int i= db.SaveChanges();
+            if (i > 0) {
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                return View(emp);
+            }
+             
+        }
+
+        public ActionResult Edit(int? id)
+        {
+            EmployeeModel emp = db.EmployeeModels.Find(id);
+            return View(emp);
+        }
+        [HttpPost]
+        public ActionResult Edit(EmployeeModel emp)
+        {
+            db.Entry(emp).State = EntityState.Modified;
+            int i = db.SaveChanges();
+            if (i > 0)
+            {
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                return View(emp);
+            }
+
+        }
+
+    }
+}
---
+++ CodeFirstApproach/Models/EmployeeContext.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+namespace CodeFirstApproach.Models
+{
+    public class EmployeeContext:DbContext
+    {
+        public EmployeeContext():base("sqlCon")
+        {
+
+        }
+        public DbSet<EmployeeModel> EmployeeModels { get; set; }
+    }
+}
