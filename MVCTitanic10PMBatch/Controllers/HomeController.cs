f8ef233de6ead69e922ab8890242b04c75c9f24b
yawar2019 <yawarali17@gmail.com>
2021-05-03 23:11:20 +0530

    committed

--- MVCTitanic10PMBatch/Controllers/HomeController.cs
+++ MVCTitanic10PMBatch/Controllers/HomeController.cs
@@ -4,20 +4,59 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Web.Security;
 
 namespace MVCTitanic10PMBatch.Controllers
 {
+    [Authorize]
     public class HomeController : Controller
     {
         // GET: Home
+       
         public ActionResult Index(int? id)
         {
             return Content(id.ToString());
         }
 
-        public ActionResult Index1( EmployeeModel emp)
+        public ActionResult Index1(EmployeeModel emp)
         {
             return Content(emp.EmpId.ToString());
+        }
+
+        [HttpGet]
+        [AllowAnonymous]
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+  
+        [HttpGet]
+        public ActionResult UserDashBoard()
+        {
+            return View();
+        }
+
+        [AllowAnonymous]
+        [HttpPost]
+        public ActionResult Login(UserDetail user)
+        {
+            EmployeeEntities db = new Models.EmployeeEntities();
+            var userinfo = db.UserDetails.Where(x => x.UserName == user.UserName).FirstOrDefault();
+            if (userinfo != null)
+            {
+                var userpwd = db.UserDetails.Where(x => x.UserName == user.UserName &&  x.Password == user.Password).FirstOrDefault();
+                if (userpwd != null)
+                {
+                    FormsAuthentication.SetAuthCookie(user.UserName, false);
+                    return RedirectToAction("UserDashBoard");
+                }
+                else
+                {
+                    return View();
+                }
+            }
+            return View();
         }
     }
 }
